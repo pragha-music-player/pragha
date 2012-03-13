0ae9313a323c018f8becffb9d9474df0e11ccc35
Pavel Vasin <rat4vier@gmail.com>
2012-03-13 14:29:15 +0400

    GtkProgressBar is no-window widget in gtk3, so wrap it in event box

--- src/gui.c
+++ src/gui.c
@@ -1833,7 +1833,7 @@ GtkWidget* create_panel(struct con_win *cwin)
 GtkWidget* create_playing_box(struct con_win *cwin)
 {
 	GtkWidget *now_playing_label,*track_length_label,*track_time_label;
-	GtkWidget *track_progress_bar;
+	GtkWidget *track_progress_bar, *track_progress_event_box;
 	GtkWidget *track_length_align, *track_time_align, *track_progress_align, *vbox_align;
 	GtkWidget *pack_vbox, *playing_hbox, *time_hbox;
 	GtkWidget *track_length_event_box, *track_playing_event_box;
@@ -1880,17 +1880,17 @@ GtkWidget* create_playing_box(struct con_win *cwin)
 	/* Setup track progress */
 
 	track_progress_align = gtk_alignment_new(0, 0.5, 1, 0);
+	track_progress_event_box = gtk_event_box_new();
 	track_progress_bar = gtk_progress_bar_new();
 
         gtk_widget_set_size_request(GTK_WIDGET(track_progress_bar),
                                       -1,
                                       12);
 
-	gtk_container_add(GTK_CONTAINER(track_progress_align), track_progress_bar);
+	gtk_container_add(GTK_CONTAINER(track_progress_align), track_progress_event_box);
+	gtk_container_add(GTK_CONTAINER(track_progress_event_box), track_progress_bar);
 
-	gtk_widget_set_events(track_progress_bar, GDK_BUTTON_PRESS_MASK);
-
-	g_signal_connect(G_OBJECT(track_progress_bar), "button-press-event",
+	g_signal_connect(G_OBJECT(track_progress_event_box), "button-press-event",
 			 G_CALLBACK(track_progress_change_cb), cwin);
 
 	track_time_label = gtk_label_new(NULL);
