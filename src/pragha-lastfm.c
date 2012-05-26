45a74c9274ea5be9d4df594a6898a8cf4cd86972
Matias De lellis <mati86dl@gmail.com>
2012-05-25 21:02:05 -0300

    D'Oh!. Test NULL strings before use strlen on all mobj tags!.

--- src/pragha-lastfm.c
+++ src/pragha-lastfm.c
@@ -706,7 +706,9 @@ void lastfm_now_playing_handler (struct con_win *cwin)
 		return;
 	}
 
-	if ((strlen(cwin->cstate->curr_mobj->tags->artist) == 0) ||
+	if ((cwin->cstate->curr_mobj->tags->artist == NULL) ||
+	    (cwin->cstate->curr_mobj->tags->title == NULL) ||
+	    (strlen(cwin->cstate->curr_mobj->tags->artist) == 0) ||
 	    (strlen(cwin->cstate->curr_mobj->tags->title) == 0))
 		return;
 
