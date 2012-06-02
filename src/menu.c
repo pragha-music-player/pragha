4e846fe59e887ba8849e68a5ee320fde03eac4d5
Pavel Vasin <rat4vier@gmail.com>
2012-06-02 20:02:10 +0400

    correct reverting null strings

--- src/menu.c
+++ src/menu.c
@@ -107,7 +107,7 @@ static void rescan_dialog_response_cb(GtkDialog *dialog,
 
 /* Generate and add the recently-used data */
 
-gboolean add_recent_file (gchar *filename)
+void add_recent_file (const gchar *filename)
 {
 	GtkRecentData recent_data;
 	gchar *uri = NULL;
@@ -116,7 +116,7 @@ gboolean add_recent_file (gchar *filename)
 
 	recent_data.mime_type = get_mime_type(filename);
 	if (recent_data.mime_type == NULL)
-		return FALSE;
+		return;
 
 	recent_data.display_name = g_filename_display_basename (filename);
 	recent_data.app_name = g_strdup (g_get_application_name ());
@@ -132,8 +132,6 @@ gboolean add_recent_file (gchar *filename)
 	g_free (recent_data.app_name);
 	g_free (recent_data.app_exec);
 	g_free (uri);
-	
-	return FALSE;
 }
 
 /* Add selected files from the file chooser to the current playlist */
@@ -159,8 +157,7 @@ void handle_selected_file(gpointer data, gpointer udata)
 		mobj = new_musicobject_from_file(data);
 		if (mobj) {
 			append_current_playlist(NULL, mobj, cwin);
-			g_idle_add_full(G_PRIORITY_LOW, (GSourceFunc) add_recent_file,
-							data, NULL);
+			add_recent_file(data);
 		}
 	}
 
