e1cec37ab337afa65459ff4d039289a92fa463dd
Matias De lellis <mati86dl@gmail.com>
2012-09-22 12:01:30 -0300

    Add pragha_album_art_get_pixbuf() and use it in notifications. Should add a property.. No?.

--- src/pragha-album-art.h
+++ src/pragha-album-art.h
@@ -60,6 +60,7 @@ void
 pragha_album_art_set_size (PraghaAlbumArt *albumart,
                            guint size);
 void pragha_album_art_set_pixbuf (PraghaAlbumArt *albumart, GdkPixbuf *pixbuf);
+GdkPixbuf *pragha_album_art_get_pixbuf (PraghaAlbumArt *albumart);
 void pragha_album_art_clear_icon (PraghaAlbumArt *albumart);
 
 G_END_DECLS
