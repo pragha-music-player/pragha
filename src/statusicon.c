12956f7670bf314b958a56391f54b0cb05e5878c
Matias De lellis <mati86dl@gmail.com>
2012-10-10 13:15:37 -0300

    Port status icon menus updates to "state-change" signal.

--- src/statusicon.c
+++ src/statusicon.c
@@ -168,10 +168,12 @@ systray_quit (GtkAction *action, struct con_win *cwin)
 }
 
 static void
-update_systray_menu (struct con_win *cwin)
+update_systray_menu_cb (GObject *gobject, gint state, gpointer user_data)
 {
 	GtkAction *action;
-	gboolean playing = (pragha_backend_get_state (cwin->backend) != ST_STOPPED);
+	struct con_win *cwin = user_data;
+
+	gboolean playing = (state != ST_STOPPED);
 
 	action = gtk_ui_manager_get_action(cwin->systray_menu, "/popup/Prev");
 	gtk_action_set_sensitive (GTK_ACTION (action), playing);
@@ -184,13 +186,6 @@ update_systray_menu (struct con_win *cwin)
 
 	action = gtk_ui_manager_get_action(cwin->systray_menu, "/popup/Edit tags");
 	gtk_action_set_sensitive (GTK_ACTION (action), playing);
-}
-
-static void
-update_systray_menu_cb (GObject *gobject, GParamSpec *pspec, gpointer user_data)
-{
-	struct con_win *cwin = user_data;
-	update_systray_menu (cwin);
 }
 
 static GtkUIManager*
@@ -250,6 +245,5 @@ void create_status_icon (struct con_win *cwin)
 	cwin->status_icon = status_icon;
 	cwin->systray_menu = systray_menu;
 
-	update_systray_menu (cwin);
-	g_signal_connect (cwin->backend, "notify::state", G_CALLBACK (update_systray_menu_cb), cwin);
+	g_signal_connect (cwin->backend, "state-change", G_CALLBACK (update_systray_menu_cb), cwin);
 }
