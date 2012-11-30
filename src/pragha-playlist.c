50be7f5a77e8288cdf4677e9a0df217314bba2d9
Matias De lellis <mati86dl@gmail.com>
2012-11-30 13:41:06 -0300

    Add only once the songs suggested by lastfm, and remove unused code.

--- src/pragha-playlist.c
+++ src/pragha-playlist.c
@@ -1928,31 +1928,26 @@ pragha_playlist_append_mobj_list(PraghaPlaylist *cplaylist, GList *list)
 	g_object_unref(model);
 }
 
-
-/* Test if the song is already in the playlist.*/
+/* Test if the song is already in the mobj list */
 
 gboolean
-pragha_playlist_already_has_file(PraghaPlaylist *cplaylist, gchar *file)
+pragha_mobj_list_already_has_title_of_artist(GList *list,
+					     const gchar *title,
+					     const gchar *artist)
 {
-	GtkTreeModel *model;
-	GtkTreeIter iter;
 	struct musicobject *mobj = NULL;
-	gboolean ret;
+	GList *i;
 
-	model = gtk_tree_view_get_model (GTK_TREE_VIEW(cplaylist->view));
-
-	ret = gtk_tree_model_get_iter_first (model, &iter);
-	while (ret) {
-		gtk_tree_model_get (model, &iter, P_MOBJ_PTR, &mobj, -1);
-
-		if(0 == g_strcmp0(mobj->file, file))
+	for (i = list; i != NULL; i = i->next) {
+		mobj = i->data;
+		if((0 == g_ascii_strcasecmp(mobj->tags->title, title)) &&
+		   (0 == g_ascii_strcasecmp(mobj->tags->artist, artist)))
 		   	return TRUE;
-
-		ret = gtk_tree_model_iter_next(model, &iter);
 	}
-
 	return FALSE;
 }
+
+/* Test if the song is already in the playlist.*/
 
 gboolean
 pragha_playlist_already_has_title_of_artist(PraghaPlaylist *cplaylist,
