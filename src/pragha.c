f327d80143f9296b0ccc79362a8faa8690612d71
Pavel Vasin <rat4vier@gmail.com>
2013-01-10 14:54:22 +0400

    fix use-after-free at exit

--- src/pragha.c
+++ src/pragha.c
@@ -36,7 +36,6 @@ static void common_cleanup(struct con_win *cwin)
 	g_object_unref (cwin->backend);
 	gui_free (cwin);
 	state_free (cwin->cstate);
-	pragha_mutex_free(cwin->cstate->curr_mobj_mutex);
 	g_object_unref(G_OBJECT(cwin->preferences));
 	preferences_free (cwin->cpref);
 	db_free (cwin->cdbase);
