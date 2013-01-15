ac6fca11db146bae5c0243159166504e11f29768
Matias De lellis <mati86dl@gmail.com>
2013-01-15 15:26:19 -0300

    Add complete audio cd in a one time.

--- src/cdda.c
+++ src/cdda.c
@@ -49,6 +49,7 @@ static void add_audio_cd_tracks(struct con_win *cwin)
 {
 	PraghaMusicobject *mobj;
 	gint num_tracks = 0, i = 0, prev_tracks = 0;
+	GList *list = NULL;
 
 	num_tracks = cdio_cddap_tracks(cwin->cstate->cdda_drive);
 	if (!num_tracks)
@@ -58,11 +59,15 @@ static void add_audio_cd_tracks(struct con_win *cwin)
 
 	for (i = 1; i <= num_tracks; i++) {
 		mobj = new_musicobject_from_cdda(cwin, i);
-		append_current_playlist(cwin->cplaylist, mobj);
+		list = g_list_append(list, mobj);
 
 		if (pragha_process_gtk_events ())
 			return;
 	}
+
+	pragha_playlist_append_mobj_list(cwin->cplaylist, list);
+	g_list_free(list);
+
 	pragha_playlist_update_statusbar_playtime(cwin->cplaylist);
 	select_numered_path_of_current_playlist(cwin->cplaylist, prev_tracks, TRUE);
 }
