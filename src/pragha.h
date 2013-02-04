776c894cd08f6449ef3fc1141ccf8da1e98bccc1
Pavel Vasin <rat4vier@gmail.com>
2013-02-04 18:53:09 +0400

    db: refactor find_genre()

--- src/pragha.h
+++ src/pragha.h
@@ -864,7 +864,6 @@ gint add_new_year_db(guint year, PraghaDatabase *cdbase);
 gint add_new_comment_db(const gchar *comment, PraghaDatabase *cdbase);
 void add_track_playlist_db(const gchar *file, gint playlist_id, PraghaDatabase *cdbase);
 void add_track_radio_db(const gchar *uri, gint radio_id, PraghaDatabase *cdbase);
-gint find_genre_db(const gchar *genre, PraghaDatabase *cdbase);
 gint find_year_db(gint year, PraghaDatabase *cdbase);
 gint find_comment_db(const gchar *comment, PraghaDatabase *cdbase);
 gint find_playlist_db(const gchar *playlist, PraghaDatabase *cdbase);
