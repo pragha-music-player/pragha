f6250e6b3cf143e6e36b9f3129052bd6f50d468f
Matias De lellis <mati86dl@gmail.com>
2013-02-16 19:15:06 -0300

    Update years copyright.

--- src/menu.c
+++ src/menu.c
@@ -1,6 +1,6 @@
 /*************************************************************************/
 /* Copyright (C) 2007-2009 sujith <m.sujith@gmail.com>			 */
-/* Copyright (C) 2009-2012 matias <mati86dl@gmail.com>			 */
+/* Copyright (C) 2009-2013 matias <mati86dl@gmail.com>			 */
 /* 									 */
 /* This program is free software: you can redistribute it and/or modify	 */
 /* it under the terms of the GNU General Public License as published by	 */
@@ -882,7 +882,7 @@ void about_widget(struct con_win *cwin)
 				"authors", authors,
 				"translator-credits", _("translator-credits"),
 				"comments", "A lightweight GTK+ music player",
-				"copyright", "(C) 2007-2009 Sujith\n(C) 2009-2012 Matias",
+				"copyright", "(C) 2007-2009 Sujith\n(C) 2009-2013 Matias",
 				"license", license,
 				"name", PACKAGE_NAME,
 				"version", PACKAGE_VERSION,
--- src/pragha-backend.c
+++ src/pragha-backend.c
@@ -1,5 +1,6 @@
 /*************************************************************************/
 /* Copyright (C) 2010-2012 matias <mati86dl@gmail.com>			 */
+/* Copyright (C) 2012-2013 Pavel Vasin					 */
 /* 									 */
 /* This program is free software: you can redistribute it and/or modify	 */
 /* it under the terms of the GNU General Public License as published by	 */
