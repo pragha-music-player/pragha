bf6a601c4d8f058092244ae7525e36635c098b4f
Matias De lellis <mati86dl@gmail.com>
2013-03-03 16:05:35 -0300

    Rename utils.c to pragha-utils.c and add pragha-utils.h

--- src/pragha.h
+++ src/pragha.h
@@ -88,7 +88,7 @@
 #include "pragha-simple-async.h"
 #include "pragha-statusbar.h"
 #include "pragha-toolbar.h"
-
+#include "pragha-utils.h"
 #include "gnome-media-keys.h"
 
 #include "gtkcellrendererbubble.h"
@@ -403,11 +403,6 @@ extern const gchar *mime_playlist[];
 extern const gchar *mime_dual[];
 #endif
 
-/* Convenience macros */
-
-#define string_is_empty(s) (!(s) || !(s)[0])
-#define string_is_not_empty(s) (s && (s)[0])
-
 /* Debugging */
 
 #define CDEBUG(_lvl, _fmt, ...)			\
@@ -555,40 +550,6 @@ DBusHandlerResult dbus_filter_handler(DBusConnection *conn,
 				      gpointer data);
 void dbus_send_signal(const gchar *signal, struct con_win *cwin);
 void dbus_handlers_free(struct con_win *cwin);
-
-/* Utilities */
-
-void pragha_log_to_file (const gchar* log_domain, GLogLevelFlags log_level, const gchar* message, gpointer user_data);
-gchar *e2_utf8_ndup (const gchar *str, glong num);
-gsize levenshtein_strcmp(const gchar * s, const gchar * t);
-gsize levenshtein_safe_strcmp(const gchar * s, const gchar * t);
-gchar *g_strstr_lv (gchar *haystack, gchar *needle, gsize lv_distance);
-gchar *pragha_strstr_lv(gchar *haystack, gchar *needle, PraghaPreferences *preferences);
-#if !GLIB_CHECK_VERSION(2,32,0)
-gboolean nm_is_online ();
-#endif
-gboolean already_in_current_playlist(PraghaMusicobject *mobj, struct con_win *cwin);
-GList *prepend_song_with_artist_and_title_to_mobj_list(const gchar *artist, const gchar *title, GList *list, struct con_win *cwin);
-void set_watch_cursor (GtkWidget *widget);
-void remove_watch_cursor (GtkWidget *widget);
-GdkPixbuf *vgdk_pixbuf_new_from_memory (const char *data, size_t size);
-enum file_type get_file_type(const gchar *file);
-gchar* get_mime_type(const gchar *file);
-enum playlist_type pragha_pl_parser_guess_format_from_extension (const gchar *filename);
-gboolean is_image_file(const gchar *file);
-gchar* convert_length_str(gint length);
-gboolean is_present_str_list(const gchar *str, GSList *list);
-GSList* delete_from_str_list(const gchar *str, GSList *list);
-gchar * path_get_dir_as_uri (const gchar *path);
-gchar* get_display_filename(const gchar *filename, gboolean get_folder);
-gchar* get_display_name(PraghaMusicobject *mobj);
-void free_str_list(GSList *list);
-gint compare_utf8_str(const gchar *str1, const gchar *str2);
-gboolean validate_album_art_pattern(const gchar *pattern);
-gboolean pragha_process_gtk_events ();
-void open_url(const gchar *url, GtkWidget *parent);
-void menu_position(GtkMenu *menu, gint *x, gint *y, gboolean *push_in, gpointer user_data);
-gboolean is_incompatible_upgrade(struct con_win *cwin);
 
 /* Some widgets. */
 
