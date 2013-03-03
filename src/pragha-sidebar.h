39bce9a7bdf61404c00d08ddde0fa6d177724c82
Matias De lellis <mati86dl@gmail.com>
2013-03-03 10:08:35 -0300

    Add pragha-sidebar.h

---
+++ src/pragha-sidebar.h
@@ -0,0 +1,41 @@
+/*************************************************************************/
+/* Copyright (C) 2013 matias <mati86dl@gmail.com>                        */
+/*                                                                       */
+/* This program is free software: you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation, either version 3 of the License, or     */
+/* (at your option) any later version.                                   */
+/*                                                                       */
+/* This program is distributed in the hope that it will be useful,       */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/* GNU General Public License for more details.                          */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/*************************************************************************/
+
+#ifndef PRAGHA_SIDEBAR_H
+#define PRAGHA_SIDEBAR_H
+
+#include <gtk/gtk.h>
+
+typedef struct {
+	GtkWidget *widget;
+	GtkWidget *container;
+	GtkWidget *header;
+	GtkWidget *menu_button;
+	GtkWidget *close_button;
+	GtkWidget *label;
+	GtkMenu *popup_menu;
+} PraghaSidebar;
+
+void           pragha_sidebar_header_set_text (PraghaSidebar *sidebar, const gchar *text);
+void           pragha_sidebar_attach_menu     (PraghaSidebar *sidebar, GtkMenu *menu);
+void           pragha_sidebar_add_pane        (PraghaSidebar *sidebar, GtkWidget *widget);
+GtkWidget     *pragha_sidebar_get_widget      (PraghaSidebar *sidebar);
+
+void           pragha_sidebar_free            (PraghaSidebar *sidebar);
+PraghaSidebar *pragha_sidebar_new             ();
+
+#endif /* PRAGHA_SIDEBAR_H */
