b3587a61619fa9b625df9a13b384ab24566ef4fd
Matias De lellis <mati86dl@gmail.com>
2013-03-05 12:40:32 -0300

    Add header file to glyr-related.c. TODO: Search a good name to rename!

--- src/glyr-related.c
+++ src/glyr-related.c
@@ -16,20 +16,13 @@
 /*************************************************************************/
 
 #include "pragha.h"
+#include "glyr-related.h"
 #include "pragha-simple-async.h"
 #include "pragha-file-utils.h"
 #include "pragha-utils.h"
 #include "pragha-simple-widgets.h"
 
 #ifdef HAVE_LIBGLYR
-
-typedef struct
-{
-	struct con_win	*cwin;
-	GlyrQuery	query;
-	GlyrMemCache	*head;
-}
-glyr_struct;
 
 /* Use the download info on glyr thread and show a dialog. */
 
