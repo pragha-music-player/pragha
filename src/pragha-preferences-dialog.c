pragha-music-player/pragha

commit b4b32eb492a38b7b23283bde53e6297db2c6db62

Notifications are made a plugin.

src/pragha-preferences-dialog.c
@@ -35,7 +35,6 @@
 #include "pragha-preferences-dialog.h"
 #include "pragha-hig.h"
 #include "pragha-utils.h"
-#include "pragha-notify.h"
 #include "pragha-simple-widgets.h"
 #include "pragha.h"
 
@@ -66,10 +65,6 @@ struct _PreferencesDialog {
 	GtkWidget *show_icon_tray_w;
 	GtkWidget *close_to_tray_w;
 	GtkWidget *add_recursively_w;
-
-	GtkWidget *show_osd_w;
-	GtkWidget *albumart_in_osd_w;
-	GtkWidget *actions_in_osd_w;
 
 #ifdef HAVE_LIBCLASTFM
 	GtkWidget *lastfm_w;
@@ -146,11 +141,10 @@ static void
 pragha_preferences_dialog_response(GtkDialog *dialog_w, gint response_id, PreferencesDialog *dialog)
 {
 	PraghaLibraryPane *library;
-	PraghaNotify *notify;
 #ifdef HAVE_LIBCLASTFM
 	PraghaLastfm *clastfm;
 #endif
-	gboolean osd, test_change, pref_setted, pref_toggled;
+	gboolean test_change, pref_setted, pref_toggled;
 	gchar *audio_sink = NULL, *window_state_sink = NULL;
 	const gchar *album_art_pattern, *audio_cd_device, *audio_device;
 	gboolean show_album_art, instant_search, approximate_search, restore_playlist, add_recursively;
@@ -323,28 +317,6 @@ pragha_preferences_dialog_response(GtkDialog *dialog_w, gint response_id, Prefer
 			}
 		}
 
-		/* Notification preferences */
-
-		osd = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(dialog->show_osd_w));
-		if (osd) {
-			pragha_preferences_set_show_osd(dialog->preferences, TRUE);
-			notify = pragha_application_get_notify (dialog->pragha);
-			if (!notify) {
-				notify = pragha_notify_new (dialog->pragha);
-				if (notify)
-					pragha_application_set_notify (dialog->pragha, notify);
-				else
-					pragha_preferences_set_show_osd(dialog->preferences, FALSE);
-			}
-		}
-		else
-			pragha_preferences_set_show_osd(dialog->preferences, FALSE);
-
-		pragha_preferences_set_album_art_in_osd (dialog->preferences,
-			gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(dialog->albumart_in_osd_w)));
-		pragha_preferences_set_actions_in_osd (dialog->preferences,
-			gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(dialog->actions_in_osd_w)));
-
 		/* Services internet preferences */
 #ifdef HAVE_LIBCLASTFM
 		pragha_preferences_set_lastfm_support (dialog->preferences,
@@ -492,8 +464,6 @@ static void toggle_album_art(GtkToggleButton *button, PreferencesDialog *dialog)
 
 	gtk_widget_set_sensitive(dialog->album_art_pattern_w, is_active);
 	gtk_widget_set_sensitive(dialog->album_art_size_w, is_active);
-
-	gtk_widget_set_sensitive(dialog->albumart_in_osd_w, is_active);
 }
 
 /* Toggle show status icon. */
@@ -507,18 +477,7 @@ static void toggle_show_icon_tray(GtkToggleButton *button, PreferencesDialog *di
 	pragha_preferences_set_show_status_icon (dialog->preferences, is_active);
 }
 
-/* Toggle album art pattern */
-
-static void toggle_show_osd(GtkToggleButton *button, PreferencesDialog *dialog)
-{
-	gboolean is_active;
-
-	is_active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(dialog->show_osd_w));
-
-	gtk_widget_set_sensitive(dialog->albumart_in_osd_w, is_active);
-	if (can_support_actions())
-		gtk_widget_set_sensitive(dialog->actions_in_osd_w, is_active);
-}
+/* Some audios toggles handlers */
 
 static void update_audio_device_alsa(PreferencesDialog *dialog)
 {
@@ -712,17 +671,6 @@ pragha_preferences_dialog_init_settings(PreferencesDialog *dialog)
 		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(dialog->fuse_folders_w), TRUE);
 	if (pragha_preferences_get_sort_by_year(dialog->preferences))
 		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(dialog->sort_by_year_w), TRUE);
-
-	/* Notifications options */
-
-	if (pragha_preferences_get_show_osd(dialog->preferences))
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(dialog->show_osd_w), TRUE);
-	if (pragha_preferences_get_album_art_in_osd(dialog->preferences))
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(dialog->albumart_in_osd_w), TRUE);
-	if (!can_support_actions())
-		gtk_widget_set_sensitive(dialog->actions_in_osd_w, FALSE);
-	else if (pragha_preferences_get_actions_in_osd(dialog->preferences))
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(dialog->actions_in_osd_w), TRUE);
 
 	/* Service Internet Option */
 #ifdef HAVE_LIBCLASTFM
@@ -1015,7 +963,6 @@ pref_create_desktop_page(PreferencesDialog *dialog)
 {
 	GtkWidget *table;
 	GtkWidget *show_icon_tray, *close_to_tray;
-	GtkWidget *show_osd, *albumart_in_osd, *actions_in_osd;
 	guint row = 0;
 
 	table = pragha_hig_workarea_table_new();
@@ -1028,31 +975,10 @@ pref_create_desktop_page(PreferencesDialog *dialog)
 	close_to_tray = gtk_check_button_new_with_label(_("Minimize Pragha when close the window"));
 	pragha_hig_workarea_table_add_wide_control(table, &row, close_to_tray);
 
-	pragha_hig_workarea_table_add_section_title(table, &row, _("Notifications"));
-
-	show_osd = gtk_check_button_new_with_label(_("Show OSD for track change"));
-	pragha_hig_workarea_table_add_wide_control(table, &row, show_osd);
-
-	albumart_in_osd = gtk_check_button_new_with_label(_("Show Album art in notifications"));
-	pragha_hig_workarea_table_add_wide_control(table, &row, albumart_in_osd);
-
-	actions_in_osd = gtk_check_button_new_with_label(_("Add actions to change track to notifications"));
-	pragha_hig_workarea_table_add_wide_control(table, &row, actions_in_osd);
-
-	/* Setup signal handlers */
-
-	g_signal_connect(G_OBJECT(show_icon_tray), "toggled",
-			 G_CALLBACK(toggle_show_icon_tray), dialog);
-	g_signal_connect(G_OBJECT(show_osd), "toggled",
-			 G_CALLBACK(toggle_show_osd), dialog);
-
 	/* Store references. */
 
 	dialog->show_icon_tray_w = show_icon_tray;
 	dialog->close_to_tray_w = close_to_tray;
-	dialog->show_osd_w = show_osd;
-	dialog->albumart_in_osd_w = albumart_in_osd;
-	dialog->actions_in_osd_w = actions_in_osd;
 
 	return table;
 }

configure.ac
@@ -80,7 +80,6 @@ XDT_CHECK_PACKAGE([GSTREAMER_BASE], [gstreamer-base-1.0], [$GSTREAMER_REQUIRED])
 XDT_CHECK_PACKAGE([GIO], [gio-2.0], [2.28])
 XDT_CHECK_PACKAGE([GTK], [gtk+-3.0], [3.2.0])
 XDT_CHECK_PACKAGE([SQLITE3], [sqlite3], [3.4])
-XDT_CHECK_PACKAGE([LIBNOTIFY], [libnotify], [0.4.4])
 XDT_CHECK_PACKAGE([LIBCDIO], [libcdio], [0.80])
 XDT_CHECK_PACKAGE([LIBCDIO_PARANOIA], [libcdio_paranoia], [0.80])
 XDT_CHECK_PACKAGE([LIBCDDB], [libcddb], [1.3.0])
@@ -95,6 +94,15 @@ XDT_CHECK_OPTIONAL_PACKAGE([LIBPEAS],
 
 if test x"$LIBPEAS_FOUND" = x"yes"; then
 XDT_CHECK_PACKAGE([LIBPEAS_GTK], [libpeas-gtk-1.0], [1.0.0])
+fi
+
+dnl Check notify support
+LIBNOTIFY_FOUND="no"
+if test x"$LIBPEAS_FOUND" = x"yes"; then
+XDT_CHECK_OPTIONAL_PACKAGE([LIBNOTIFY],
+                           [libnotify], [0.4.4],
+                           [libnotify],
+                           [libnotify library], [yes])
 fi
 
 dnl Check global keyboard shortcuts support
@@ -167,6 +175,7 @@ AC_CONFIG_FILES([plugins/mpris2/Makefile])
 AC_CONFIG_FILES([plugins/keybinder/Makefile])
 AC_CONFIG_FILES([plugins/gnome-media-keys/Makefile])
 AC_CONFIG_FILES([plugins/song-info/Makefile])
+AC_CONFIG_FILES([plugins/notify/Makefile])
 fi
 AC_OUTPUT
 
@@ -178,6 +187,7 @@ echo "  Prefix................: ${prefix}"
 echo "  Debug enabled.........: $debug"
 echo ""
 echo "  Plugins enabled.......: $LIBPEAS_FOUND"
+echo "   * Show notification when change songs....................................: $LIBNOTIFY_FOUND"
 echo "   * Controls Pragha using Mpris2 interface.................................: $LIBPEAS_FOUND"
 echo "   * Controls Pragha using gnome-media-keys interface.......................: $LIBPEAS_FOUND"
 echo "   * Controls Pragha using multimedia keys.............(Need keybinder-3.0).: $LIBKEYBINDER_FOUND"

plugins/Makefile.am
@@ -21,3 +21,6 @@ if HAVE_LIBKEYBINDER
 SUBDIRS += keybinder
 endif
 
+if HAVE_LIBNOTIFY
+SUBDIRS += notify
+endif

plugins/gnome-media-keys/Makefile.am
@@ -21,7 +21,6 @@ libgnome_media_keys_la_CFLAGS =						\
 	$(GTK_CFLAGS) 							\
 	$(SQLITE3_CFLAGS) 						\
 	$(TAGLIB_C_CFLAGS) 						\
-	$(LIBNOTIFY_CFLAGS) 						\
 	$(LIBPEAS_CFLAGS) 						\
 	$(LIBPEAS_GTK_CFLAGS) 						\
 	$(LIBCDIO_CFLAGS) 						\
@@ -36,7 +35,6 @@ libgnome_media_keys_la_LIBADD =						\
 	$(GTK_LIBS) 							\
 	$(SQLITE3_LIBS)							\
 	$(TAGLIB_C_LIBS)						\
-	$(LIBNOTIFY_LIBS) 						\
 	$(LIBPEAS_LIBS)							\
 	$(LIBPEAS_GTK_LIBS)						\
 	$(LIBCDIO_LIBS) 						\

plugins/keybinder/Makefile.am
@@ -22,7 +22,6 @@ libkeybinder_la_CFLAGS =						\
 	$(GTK_CFLAGS) 							\
 	$(SQLITE3_CFLAGS) 						\
 	$(TAGLIB_C_CFLAGS) 						\
-	$(LIBNOTIFY_CFLAGS) 						\
 	$(LIBPEAS_CFLAGS) 						\
 	$(LIBPEAS_GTK_CFLAGS) 						\
 	$(LIBCDIO_CFLAGS) 						\
@@ -38,7 +37,6 @@ libkeybinder_la_LIBADD =						\
 	$(GTK_LIBS) 							\
 	$(SQLITE3_LIBS)							\
 	$(TAGLIB_C_LIBS)						\
-	$(LIBNOTIFY_LIBS) 						\
 	$(LIBPEAS_LIBS)							\
 	$(LIBPEAS_GTK_LIBS)						\
 	$(LIBCDIO_LIBS) 						\

plugins/mpris2/Makefile.am
@@ -21,7 +21,6 @@ libmpris2_la_CFLAGS =						\
 	$(GTK_CFLAGS) 							\
 	$(SQLITE3_CFLAGS) 						\
 	$(TAGLIB_C_CFLAGS) 						\
-	$(LIBNOTIFY_CFLAGS) 						\
 	$(LIBPEAS_CFLAGS) 						\
 	$(LIBPEAS_GTK_CFLAGS) 						\
 	$(LIBCDIO_CFLAGS) 						\
@@ -36,7 +35,6 @@ libmpris2_la_LIBADD =						\
 	$(GTK_LIBS) 							\
 	$(SQLITE3_LIBS)							\
 	$(TAGLIB_C_LIBS)						\
-	$(LIBNOTIFY_LIBS) 						\
 	$(LIBPEAS_LIBS)							\
 	$(LIBPEAS_GTK_LIBS)						\
 	$(LIBCDIO_LIBS) 						\

plugins/notify/Makefile.am
@@ -1,20 +1,18 @@
-plugindir = $(libdir)/pragha/plugins/song-info
+plugindir = $(libdir)/pragha/plugins/notify
 
 AM_CPPFLAGS = \
 	-I$(top_srcdir) 	\
 	$(LIBPEAS_CFLAGS)		\
 	$(LIBPEAS_GTK_CFLAGS)
 
-plugin_LTLIBRARIES = libsong-info.la
+plugin_LTLIBRARIES = libnotify.la
 
-libsong_info_la_SOURCES =						\
-	pragha-song-info-plugin.c					\
-	pragha-song-info-dialog.c					\
-	pragha-song-info-dialog.h
+libnotify_la_SOURCES =						\
+	pragha-notify-plugin.c
 
-libsong_info_la_LDFLAGS = $(PLUGIN_LIBTOOL_FLAGS)
+libnotify_la_LDFLAGS = $(PLUGIN_LIBTOOL_FLAGS)
 
-libsong_info_la_CFLAGS =						\
+libnotify_la_CFLAGS =						\
 	$(LIBGLYR_CFLAGS)							\
 	$(GSTREAMER_CFLAGS)						\
 	$(GSTREAMER_BASE_CFLAGS)					\
@@ -23,6 +21,7 @@ libsong_info_la_CFLAGS =						\
 	$(GTK_CFLAGS) 							\
 	$(SQLITE3_CFLAGS) 						\
 	$(TAGLIB_C_CFLAGS) 						\
+	$(LIBNOTIFY_CFLAGS) 						\
 	$(LIBPEAS_CFLAGS) 						\
 	$(LIBPEAS_GTK_CFLAGS) 						\
 	$(LIBCDIO_CFLAGS) 						\
@@ -30,7 +29,7 @@ libsong_info_la_CFLAGS =						\
 	-I$(top_srcdir)/src/include 					\
 	-Wall
 
-libsong_info_la_LIBADD =						\
+libnotify_la_LIBADD =						\
 	$(LIBGLYR_LIBS) 							\
 	$(GSTREAMER_LIBS)							\
 	$(GLIB_LIBS) 							\
@@ -38,6 +37,7 @@ libsong_info_la_LIBADD =						\
 	$(GTK_LIBS) 							\
 	$(SQLITE3_LIBS)							\
 	$(TAGLIB_C_LIBS)							\
+	$(LIBNOTIFY_LIBS) 						\
 	$(LIBPEAS_LIBS)							\
 	$(LIBPEAS_GTK_LIBS)						\
 	$(LIBCDIO_LIBS) 							\
@@ -45,15 +45,15 @@ libsong_info_la_LIBADD =						\
 	$(LIBCDDB_LIBS)
 
 if HAVE_GSTREAMER_AUDIO
-libsong_info_la_CFLAGS += $(GSTREAMER_AUDIO_CFLAGS)
-libsong_info_la_LIBADD += $(GSTREAMER_AUDIO_LIBS)
+libnotify_la_CFLAGS += $(GSTREAMER_AUDIO_CFLAGS)
+libnotify_la_LIBADD += $(GSTREAMER_AUDIO_LIBS)
 endif
 
 if HAVE_LIBXFCE4UI
-libsong_info_la_CFLAGS += $(LIBXFCE4UI_CFLAGS)
-libsong_info_la_LIBADD += $(LIBXFCE4UI_LIBS)
+libnotify_la_CFLAGS += $(LIBXFCE4UI_CFLAGS)
+libnotify_la_LIBADD += $(LIBXFCE4UI_LIBS)
 endif
 
-plugin_DATA = song-info.plugin
+plugin_DATA = notify.plugin
 
 EXTRA_DIST = $(plugin_DATA)

plugins/notify/notify.plugin
@@ -1,0 +1,9 @@
+[Plugin]
+Module=notify
+Name=Notification Plugin
+Description=Show notification when change songs
+Authors=Matias De lellis <mati86dl@gmail.com>
+Copyright=Copyright © 2014 Matias De lellis
+Website=https://github.com/matiasdelellis/pragha
+Help=https://github.com/matiasdelellis/pragha
+

commit 231bd55138630b69957938fb547248d58d8e7575

D'Oh!. Fix hiding status icon broken since b4b32eb4

src/pragha-preferences-dialog.c
@@ -975,6 +975,11 @@ pref_create_desktop_page(PreferencesDialog *dialog)
 	close_to_tray = gtk_check_button_new_with_label(_("Minimize Pragha when close the window"));
 	pragha_hig_workarea_table_add_wide_control(table, &row, close_to_tray);
 
+	/* Setup signal handlers */
+
+	g_signal_connect (G_OBJECT(show_icon_tray), "toggled",
+	                  G_CALLBACK(toggle_show_icon_tray), dialog);
+
 	/* Store references. */
 
 	dialog->show_icon_tray_w = show_icon_tray;