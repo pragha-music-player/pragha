eb0f5dd95a2ea1444182bfd47a2be98966f890f6
rski <rom.skiad@gmail.com>
2015-11-30 20:01:33 +0200

    Style fixes, fix setting gboolean to opposite of what it should be

--- src/info-bar-import-music.c
+++ src/info-bar-import-music.c
@@ -33,10 +33,10 @@
 gboolean info_bar_import_music_will_be_useful(PraghaApplication *pragha)
 {
 
-	const char *home = getenv("HOME");
+	const char *home = getenv ("HOME");
 	const char *dir = g_get_user_special_dir (G_USER_DIRECTORY_MUSIC);
 	//if G_USER_DIRECTORY_MUSIC equals $HOME then it is not set
-	gboolean is_g_user_directory_music_set = (strcmp(dir,home) == 0);
+	gboolean is_g_user_directory_music_set = !(strcmp (dir,home) == 0);
 	return pragha_application_is_first_run (pragha) &&
 	         is_g_user_directory_music_set &&
 	         dir;
