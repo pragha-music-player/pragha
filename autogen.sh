52d506c4e2bacfcbad2a4d972efa7161d50cf7bf
Nisankh Acharjya <36255122+acharjyanisankh@users.noreply.github.com>
2018-05-18 23:18:04 +0530

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,6 @@ echo "Running Xfce Developer Tools..."
 xdt-autogen $@
 if [ $? -ne 0 ]; then
   echo "xdt-autogen Failed"
-  echo "Prease, install xfce4-dev-tools"
+  echo "Please, install xfce4-dev-tools"
   echo "or verify Errors"
 fi
