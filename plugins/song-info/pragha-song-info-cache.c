f89346474619d268374e3f588e6b1e7cfdc727bf
Matias De lellis <mati86dl@gmail.com>
2019-07-26 19:58:57 -0300

    D'Oh. Fix ghost row (null null), in the list of similar songs

--- plugins/song-info/pragha-song-info-cache.c
+++ plugins/song-info/pragha-song-info-cache.c
@@ -1,19 +1,19 @@
-/*
- * Copyright (C) 2018 matias <mati86dl@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*************************************************************************/
+/* Copyright (C) 2018-2019 matias <mati86dl@gmail.com>                   */
+/*                                                                       */
+/* This program is free software: you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation, either version 3 of the License, or     */
+/* (at your option) any later version.                                   */
+/*                                                                       */
+/* This program is distributed in the hope that it will be useful,       */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/* GNU General Public License for more details.                          */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/*************************************************************************/
 
 #include "pragha-song-info-cache.h"
 
@@ -176,7 +176,7 @@ pragha_info_cache_get_similar_songs (PraghaInfoCache *cache,
 
 	length = g_key_file_get_integer (key_file, "Similar-Songs", "NumberOfEntries", NULL);
 
-	for (i = 0 ; i <= length ; i++)
+	for (i = 1 ; i <= length ; i++)
 	{
 		key = g_strdup_printf("File%d", i);
 		ifile = g_key_file_get_string (key_file, "Similar-Songs", key, NULL);
